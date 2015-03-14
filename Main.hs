f8b9afb3e2821a82d9ca0d7bd31e58202d7b930b
James Baxter <jebaxter@hotmail.co.uk>
2015-03-14 13:00:35 +0000

    Add Main and cabal file

---
+++ Main.hs
@@ -0,0 +1,48 @@
+import Control.Monad
+
+import Data.List
+import qualified Data.MarkovChain as MarkovChain
+import Data.MarkovChain (MarkovChain, TwoGram(TwoGram))
+
+import System.Console.GetOpt
+import System.Environment
+import System.IO.Strict
+import System.Random
+
+import Prelude hiding (readFile)
+
+data Options = Options {
+      optWords :: Int
+    }
+
+options :: [OptDescr (Options -> Options)]
+options = 
+    [ Option ['w', 'n'] 
+                 ["words"] 
+                 (ReqArg (\ n opts -> opts{optWords=read n}) "INT")
+                 "number of words to output" ]
+
+defaultOpts :: Options
+defaultOpts = Options {
+                optWords = 0
+              }
+
+parseOpts :: IO (Options, [FilePath])
+parseOpts = do args <- getArgs
+               case getOpt Permute options args of
+                 (opts, files, []) -> 
+                     return (foldl (flip ($)) defaultOpts opts, files)
+                 (_, _, errs)      -> 
+                     ioError . userError $ unlines errs ++ usageInfo "" options
+
+addLine :: MarkovChain TwoGram String -> [String] -> MarkovChain TwoGram String
+addLine m = snd . foldl' MarkovChain.addElement (TwoGram ("",""), m)
+
+main :: IO ()
+main = do (opts, files) <- parseOpts
+          input <- liftM unlines $ mapM readFile files
+          let input' = map words $ lines input
+          let mc = foldl' addLine MarkovChain.empty input'
+          g <- newStdGen
+          let text = unfoldr (MarkovChain.nextElement mc) (TwoGram ("",""), g)
+          putStrLn . unwords $ take (optWords opts) text
