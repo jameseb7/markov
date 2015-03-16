44f8d4cd672791031f4ef08a4f40da957983c23f
James Baxter <jebaxter@hotmail.co.uk>
2015-03-16 19:32:57 +0000

    Simplify NGram and add an option for n-gram size

--- Data/MarkovChain.hs
+++ Data/MarkovChain.hs
@@ -1,5 +1,6 @@
 module Data.MarkovChain where
 
+    import Data.Default
     import Data.List
     import qualified Data.Map as Map
     import Data.Map(Map)
@@ -8,46 +9,39 @@ module Data.MarkovChain where
 
     import System.Random
 
-    class NGram n where
-        shiftAppend :: n a -> a -> n a
+    newtype NGram a = NGram [a] deriving (Eq, Ord, Show)
 
-    newtype OneGram a = OneGram a deriving (Eq, Ord, Show)
-    instance NGram OneGram where
-        shiftAppend (OneGram _) = OneGram
+    shiftAppend :: NGram a -> a -> NGram a
+    shiftAppend (NGram [])     y = NGram [y]
+    shiftAppend (NGram (_:xs)) y = NGram (xs ++ [y])
 
-    newtype TwoGram a = TwoGram (a,a) deriving (Eq, Ord, Show)
-    instance NGram TwoGram where
-        shiftAppend (TwoGram (_,a)) b = TwoGram (a,b)
+    emptyNGram :: (Default a) => Int -> NGram a
+    emptyNGram n = NGram (replicate n def)
 
-    newtype ThreeGram a = ThreeGram (a,a,a) deriving (Eq, Ord, Show)
-    instance NGram ThreeGram where
-        shiftAppend (ThreeGram (_,a,b)) c = ThreeGram (a,b,c)
+    newtype MarkovChain a = MarkovChain (Map (NGram a) (RandSet Int a))
 
-    newtype MarkovChain n a = MarkovChain (Map (n a) (RandSet Int a))
-    
-    empty :: MarkovChain n a
+    empty :: MarkovChain a
     empty = MarkovChain Map.empty
 
-    null :: MarkovChain n a -> Bool
+    null :: MarkovChain a -> Bool
     null (MarkovChain m) = Map.null m
 
-    addElement :: (NGram n, Ord (n a), Ord a) =>
-                  (n a, MarkovChain n a) -> a -> (n a, MarkovChain n a)
+    addElement :: (Ord a) => 
+                  (NGram a, MarkovChain a) -> a -> (NGram a, MarkovChain a)
     addElement (n, MarkovChain m) a = (n', MarkovChain m')
         where n' = shiftAppend n a
               m' = case Map.lookup n m of
                      Nothing -> Map.insert n (RandSet.add (a,1) RandSet.empty) m
                      Just r  -> Map.insert n (RandSet.add (a,1) r) m
 
-    nextElement :: (RandomGen g, NGram n, Ord (n a)) => 
-                   MarkovChain n a -> (n a, g) -> Maybe (a, (n a, g))
+    nextElement :: (RandomGen g, Ord a) => 
+                   MarkovChain a -> (NGram a, g) -> Maybe (a, (NGram a, g))
     nextElement (MarkovChain m) (n,g) = fmap choose $ Map.lookup n m
         where choose r = (a, (shiftAppend n a, g'))
                   where (g',a) = RandSet.randomChoice g r
 
-    construct1GramMC :: [String] -> MarkovChain OneGram String
-    construct1GramMC = snd . foldl' addElement (OneGram "", empty)
+    construct1GramMC :: [String] -> MarkovChain String
+    construct1GramMC = snd . foldl' addElement (emptyNGram 1, empty)
 
-    generateFrom1GramMC :: (RandomGen g) => 
-                           MarkovChain OneGram String -> g -> [String]
-    generateFrom1GramMC m g = unfoldr (nextElement m) (OneGram "", g)
+    generateFrom1GramMC :: (RandomGen g) => MarkovChain String -> g -> [String]
+    generateFrom1GramMC m g = unfoldr (nextElement m) (emptyNGram 1, g)
--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ import Control.Monad
 
 import Data.List
 import qualified Data.MarkovChain as MarkovChain
-import Data.MarkovChain (MarkovChain, TwoGram(TwoGram))
+import Data.MarkovChain (MarkovChain, emptyNGram)
 
 import System.Console.GetOpt
 import System.Environment
@@ -12,18 +12,23 @@ import System.Random
 import Prelude hiding (readFile)
 
 data Options = Options {
-      optWords :: Int
+      optWords :: Int,
+      optNGramSize :: Int
     }
 
 options :: [OptDescr (Options -> Options)]
 options = 
-    [ Option "wn" ["words"] 
+    [ Option "w" ["words"] 
                  (ReqArg (\ n opts -> opts{optWords=read n}) "INT")
-                 "number of words to output" ]
+                 "number of words to output" 
+    , Option "n" ["ngram-size"] 
+                 (OptArg (\ n opts -> opts{optNGramSize = maybe 2 read n}) "INT")
+                 "length of the prefix that the next word depends on" ]
 
 defaultOpts :: Options
 defaultOpts = Options {
-                optWords = 0
+                optWords = 0,
+                optNGramSize = 2
               }
 
 parseOpts :: IO (Options, [FilePath])
@@ -34,14 +39,15 @@ parseOpts = do args <- getArgs
                  (_, _, errs)      -> 
                      ioError . userError $ unlines errs ++ usageInfo "" options
 
-addLine :: MarkovChain TwoGram String -> [String] -> MarkovChain TwoGram String
-addLine m = snd . foldl' MarkovChain.addElement (TwoGram ("",""), m)
+addLine :: Int -> MarkovChain String -> [String] -> MarkovChain String
+addLine n m = snd . foldl' MarkovChain.addElement (emptyNGram n, m)
 
 main :: IO ()
 main = do (opts, files) <- parseOpts
           input <- liftM unlines $ mapM readFile files
+          let n = optNGramSize opts
           let input' = map words $ lines input
-          let mc = foldl' addLine MarkovChain.empty input'
+          let mc = foldl' (addLine n) MarkovChain.empty input'
           g <- newStdGen
-          let text = unfoldr (MarkovChain.nextElement mc) (TwoGram ("",""), g)
+          let text = unfoldr (MarkovChain.nextElement mc) (emptyNGram n, g)
           putStrLn . unwords $ take (optWords opts) text
